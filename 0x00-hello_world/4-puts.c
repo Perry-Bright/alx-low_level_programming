a9e59431f3fd4ef82baafdb0bcffe4705ef0fe41
Perry-Bright <perry16bright2004@gmail.com>
2023-03-20 09:39:09 -0700

    polished the c [rpgramming

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * here is my first comment in this c programming journey
+ * we are writing a programme that's prints a line witht he puts function
+ * Return: 0(returns 0 when program succeeds)
+ **/
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle \n");
+	return (0);
+}
