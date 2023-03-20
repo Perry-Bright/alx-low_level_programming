a25363d782bf94dca5bab7eac923a14e8db5342f
Perry-Bright <perry16bright2004@gmail.com>
2023-03-20 14:36:15 -0700

    changed content of puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
- * here is my first comment in this c programming journey
- * we are writing a programme that's prints a line witht he puts function
- * Return: 0(returns 0 when program succeeds)
- **/
+ * main - a program that prints a line with puts function
+ * Return : 0 (Success)
+ */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle \n");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
